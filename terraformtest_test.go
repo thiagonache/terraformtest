d97f5032d13ab6c0369fe128707eb8f8dc3af542
Thiago Nache Carvalho <thiagonbcarvalho@gmail.com>
2021-02-14 00:44:24 -0300

    Fix tests

--- terraformtest_test.go
+++ terraformtest_test.go
@@ -45,17 +45,17 @@ func TestTFAWS101NatEIPOne(t *testing.T) {
 	t.Parallel()
 
 	want := terraformtest.TFResource{
-		Filter: `planned_values.root_module.child_modules.#.resources.0`,
+		Filter: `planned_values.root_module.child_modules.#.resources`,
 		Check: map[string]string{
-			"0.address":               "module.vpc.aws_eip.nat[0]",
-			"0.type":                  "aws_eip",
-			"0.values.vpc":            "true",
-			"0.values.tags.Terraform": "true",
-			"0.values.timeouts":       "",
+			"0.0.address":               "module.vpc.aws_eip.nat[0]",
+			"0.0.type":                  "aws_eip",
+			"0.0.values.vpc":            "true",
+			"0.0.values.tags.Terraform": "true",
+			"0.0.values.timeouts":       "",
 		},
 	}
 
-	got, err := terraformtest.ReadTfPlan("terraform-aws-101.tfplan.json")
+	got, err := terraformtest.ReadTfPlan("testdata/terraform-aws-101.tfplan.json")
 	if err != nil {
 		t.Fatalf("cannot read terraform plan: %v", err)
 	}
@@ -72,15 +72,15 @@ func TestTFAWS101DBOptionGroup(t *testing.T) {
 	want := terraformtest.TFResource{
 		Filter: `planned_values.root_module.child_modules.#.child_modules.#.resources`,
 		Check: map[string]string{
-			"address":              "module.db.module.db_option_group.aws_db_option_group.this[0]",
-			"type":                 "aws_db_option_group",
-			"engine_name":          "mysql",
-			"major_engine_version": "5.7",
-			"option.option_name":   "MARIADB_AUDIT_PLUGIN",
+			"0.0.0.address":                     "module.db.module.db_option_group.aws_db_option_group.this[0]",
+			"0.0.0.type":                        "aws_db_option_group",
+			"0.0.0.values.engine_name":          "mysql",
+			"0.0.0.values.major_engine_version": "5.7",
+			"0.0.0.values.option.0.option_name": "MARIADB_AUDIT_PLUGIN",
 		},
 	}
 
-	got, err := terraformtest.ReadTfPlan("terraform-aws-101.tfplan.json")
+	got, err := terraformtest.ReadTfPlan("testdata/terraform-aws-101.tfplan.json")
 	if err != nil {
 		t.Fatalf("cannot read terraform plan: %v", err)
 	}
